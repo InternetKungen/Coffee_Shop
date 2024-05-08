968a84388dc6fb89e30d88bac6ba190ce83cb171
Kim Andersson <kandersson@gmail.com>
2024-05-08 14:20:17 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import AddItem from './components/AddItem'
+// import Input from './components/Input'
+// import PersonsList from './components/PersonsList'
+import SignIn from './components/SignIn'
+
+function App() {
+
+  return (
+    <>
+     {/* <Input />
+     <PersonsList /> */}
+     <SignIn />
+     <AddItem />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from './config/firebaseConfig.ts';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+export {db, auth};
