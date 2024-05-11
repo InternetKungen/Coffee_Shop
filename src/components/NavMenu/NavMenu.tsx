5854948410d97e33e556ede1874017f6b9763e92
Kim Andersson <kandersson@gmail.com>
2024-05-11 12:51:40 +0200

    Added - click, click outside, scroll will close menu

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,17 +1,38 @@
 //NavMenu.tsx
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NavMenu.module.css';
 
 const NavMenu: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const navRef = useRef<HTMLDivElement>(null);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleClickOutside = (event : any) => {
+        if (navRef.current && !navRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    const handleScroll = () => {
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className={isOpen ? styles.open : ''}>
+        <nav className={isOpen ? styles.open : ''} ref={navRef}>
             <div className={isOpen ? styles['hamburger-menu'] + ' ' + styles.open : styles['hamburger-menu']} onClick={toggleMenu}>
                 <div className={styles.line}></div>
                 <div className={styles.line}></div>
@@ -24,10 +45,10 @@ const NavMenu: React.FC = () => {
             {isOpen && (
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link to="/" onClick={toggleMenu}>Home</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link to="/about" onClick={toggleMenu}>About</Link>
                     </li>
                 </ul>
             )}
