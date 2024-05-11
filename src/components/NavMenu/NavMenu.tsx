36d86f3cfe46123acf69e3704350781a932d7a14
Kim Andersson <kandersson@gmail.com>
2024-05-12 01:57:13 +0200

    Separated hamburger-menu-button from nav-section

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -6,12 +6,16 @@ import styles from './NavMenu.module.css';
 const NavMenu: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
     const navRef = useRef<HTMLDivElement>(null);
+    const menuRef = useRef<HTMLDivElement>(null);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
-    const handleClickOutside = (event : any) => {
+    const handleClickOutside = (event: any) => {
+        if (menuRef.current && menuRef.current.contains(event.target)) {
+            return;
+        }
         if (navRef.current && !navRef.current.contains(event.target)) {
             setIsOpen(false);
         }
@@ -32,27 +36,41 @@ const NavMenu: React.FC = () => {
     }, []);
 
     return (
-        <nav className={isOpen ? styles.open : ''} ref={navRef}>
-            <div className={isOpen ? styles['hamburger-menu'] + ' ' + styles.open : styles['hamburger-menu']} onClick={toggleMenu}>
+        <>
+            <div
+                ref={menuRef}
+                className={
+                    isOpen
+                        ? styles['hamburger-menu'] + ' ' + styles.open
+                        : styles['hamburger-menu']
+                }
+                onClick={toggleMenu}
+            >
                 <div className={styles.line}></div>
                 <div className={styles.line}></div>
                 <div className={styles.line}></div>
             </div>
-            <div className={styles['nav-menu-header']}>
-                <p>Coffe Palace</p>
-                <button>🔍</button>
-            </div>
-            {isOpen && (
-                <ul>
-                    <li>
-                        <Link to="/" onClick={toggleMenu}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about" onClick={toggleMenu}>About</Link>
-                    </li>
-                </ul>
-            )}
-        </nav>
+            <nav className={isOpen ? styles.open : ''} ref={navRef}>
+                <div className={styles['nav-menu-header']}>
+                    <p>Coffe Palace</p>
+                    <button>🔍</button>
+                </div>
+                {isOpen && (
+                    <ul>
+                        <li>
+                            <Link to="/" onClick={toggleMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about" onClick={toggleMenu}>
+                                About
+                            </Link>
+                        </li>
+                    </ul>
+                )}
+            </nav>
+        </>
     );
 };
 
