543e8aff97d3a3323cd7cc1aa8518e2444fd0030
IgorGryp <igor.gryp@gmail.com>
2024-05-21 18:46:46 +0200

    made logo workig as link to the home page

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
 import LogoImg from '../../assets/img/logo-02.png';
 import styles from './Logo.module.css';
 
 function Logo() {
-    return <img src={LogoImg} alt="Logo" className={styles['logo-img']} />;
+    return (
+        <Link to="/">
+            <img src={LogoImg} alt="Logo" className={styles['logo-img']} />
+        </Link>
+    );
 }
 
 export default Logo;
