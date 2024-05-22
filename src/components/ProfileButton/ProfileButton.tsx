23f912faa303a3f2bc1c8691da9431dfc0f55d7c
Kim Andersson <kandersson@gmail.com>
2024-05-22 22:32:25 +0200

    updated path users/{userId}/profile/profile -> users/{userId}

--- src/components/ProfileButton/ProfileButton.tsx
+++ src/components/ProfileButton/ProfileButton.tsx
@@ -1,3 +1,46 @@
+// import React, { useEffect, useState } from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import { auth, db } from '../../main';
+// import ProfileMenu from '../ProfileMenu/ProfileMenu';
+// import styles from './ProfileButton.module.css';
+// import { doc, getDoc } from 'firebase/firestore';
+
+// const ProfileButton: React.FC = () => {
+//     const [user] = useAuthState(auth);
+//     const [firstName, setFirstName] = useState<string | null>(null);
+//     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//     useEffect(() => {
+//         const fetchUserProfile = async () => {
+//             if (user) {
+//                 const docRef = doc(db, 'users', user.uid, 'profile', 'profile');
+//                 const docSnap = await getDoc(docRef);
+//                 if (docSnap.exists()) {
+//                     const userProfile = docSnap.data();
+//                     setFirstName(userProfile.firstName);
+//                 } else {
+//                     console.log('No such document!');
+//                 }
+//             }
+//         };
+
+//         fetchUserProfile();
+//     }, [user]);
+
+//     const toggleMenu = () => {
+//         setIsMenuOpen(!isMenuOpen);
+//     };
+
+//     return (
+//         <div className={styles.profileButton}>
+//             <button onClick={toggleMenu}>{firstName || user?.email}</button>
+//             {isMenuOpen && <ProfileMenu />}
+//         </div>
+//     );
+// };
+
+// export default ProfileButton;
+
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../../main';
@@ -13,7 +56,7 @@ const ProfileButton: React.FC = () => {
     useEffect(() => {
         const fetchUserProfile = async () => {
             if (user) {
-                const docRef = doc(db, 'users', user.uid, 'profile', 'profile');
+                const docRef = doc(db, 'users', user.uid);
                 const docSnap = await getDoc(docRef);
                 if (docSnap.exists()) {
                     const userProfile = docSnap.data();
