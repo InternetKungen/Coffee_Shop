cfa4f18a01a3e48a10b8c985cfd1cfd4c0bb16d4
Kim Andersson <kandersson@gmail.com>
2024-05-31 14:28:26 +0200

    sign in fixed -

--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -1,67 +1,4 @@
-// // SignIn.tsx
-// import { useState } from 'react';
-// import { signInWithEmailAndPassword } from 'firebase/auth';
-// import { auth } from '../../../main';
-// import styles from './SignIn.module.css';
-// import TitleSection from '../../../components/TitleSection/TitleSection';
-// import { Link } from 'react-router-dom';
-
-// export const SignIn = () => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const signInUser = async () => {
-//         signInWithEmailAndPassword(auth, email, password)
-//             .then((userCredential) => {
-//                 console.log('inloggad');
-//             })
-//             .catch((error) => {
-//                 console.log(error.message);
-//             });
-//     };
-
-//     return (
-//         <>
-//             <section className={styles['sign-in-container']}>
-//                 <h2>Sign In</h2>
-//                 <section className={styles['sign-in-container__input-fields']}>
-//                     <label htmlFor="userEmail">E-mail</label>
-//                     <input
-//                         id="userEmail"
-//                         type="text"
-//                         value={email}
-//                         onChange={(e) => setEmail(e.target.value)}
-//                     ></input>
-//                     <br></br>
-//                     <label htmlFor="userPassword">Password</label>
-//                     <input
-//                         id="userPassword"
-//                         type="password"
-//                         value={password}
-//                         onChange={(e) => setPassword(e.target.value)}
-//                     ></input>
-//                 </section>
-//                 <br></br>
-
-//                 <button onClick={signInUser}>Sign In</button>
-
-//                 <section className={styles['sign-in-container__info']}>
-//                     <p>
-//                         Don't have an account?{' '}
-//                         <Link to="/sign-up"> Create one here</Link>
-//                     </p>
-//                     <p>
-//                         Forgot password?
-//                         <Link to="/sign-up"> Press here</Link>
-//                     </p>
-//                 </section>
-//             </section>
-//         </>
-//     );
-// };
-
-// export default SignIn;
-
+// SignIn.tsx
 import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../../main';
@@ -75,7 +12,7 @@ export const SignIn = () => {
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    const signInUser = async (e) => {
+    const signInUser = async (e: any) => {
         e.preventDefault(); // Prevent default form submission
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
